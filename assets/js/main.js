7735d18543c4f83268e2ab29563a8c99fe1019c9
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2021-07-15 08:40:14 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,49 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     let inptValorConta = document.querySelector('#bill');
 
     let btnPorcentagem = document.querySelectorAll('.button__tip');
-   
+
     let inptPessoas = document.querySelector('#peoples');
 
 
-    btnPorcentagem.forEach((botoes)=>{
+    btnPorcentagem.forEach((botoes) => {
         botoes.addEventListener('click', calculoPorcentagem);
         console.log(botoes.value)
     })
-    function calculoPorcentagem(event){
+
+
+    function calculoPorcentagem(event) {
         const valorConta = Number(inptValorConta.value);
         const qtdPessoas = Number(inptPessoas.value);
         const valorBotao = Number(this.value);
-        
 
-        let calculo = (valorConta * valorBotao) / 100;
+        if (valorConta <= 0 || qtdPessoas <= 0) {
+            console.log('error')
+            document.body.classList.add('error');
+            inptValorConta.nextElementSibling.innerHTML = "todos os campos devem estar preenchidos";
+            inptPessoas.nextElementSibling.innerHTML = "todos os campos devem estar preenchidos";
+        } else {
 
-        let divisaoPessoas = Number(Math.round(calculo / qtdPessoas));
+            let calculo = (valorConta * valorBotao) / 100;
 
-        function mostrarLCD(){
-            const tipInfo = document.querySelector('#valorGorjeta');
-            const valorDivido = document.querySelector('#valorTotal');
-            tipInfo.innerHTML = calculo;
-            valorDivido.innerHTML = divisaoPessoas;
+            let divisaoPessoas = Number(Math.round(calculo / qtdPessoas));
+
+            function mostrarLCD() {
+                const tipInfo = document.querySelector('#valorGorjeta');
+                const valorDivido = document.querySelector('#valorTotal');
+                tipInfo.innerHTML = `$${calculo}`;
+                valorDivido.innerHTML = `$${divisaoPessoas}`;
+            }
+
+
+            mostrarLCD();
+
         }
 
-        mostrarLCD();
-
     }
-    
+
+
+
 
 })
 
